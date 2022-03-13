f78b40a54477ada1b1b06da3c14b410d3c44a98f
bigfarts <bigfarts@punymail.com>
2022-03-13 16:41:04 -0700

    whoops

--- chips/chips.go
+++ chips/chips.go
@@ -6,127 +6,92 @@ import (
 )
 
 var Chips = []*state.Chip{
-	// {
-	// 	Index:      0,
-	// 	Name:       "Cannon",
-	// 	BaseDamage: 40,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Cannon{Style: behaviors.CannonStyleCannon, Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      1,
-	// 	Name:       "HiCannon",
-	// 	BaseDamage: 100,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Cannon{Style: behaviors.CannonStyleHiCannon, Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      2,
-	// 	Name:       "M-Cannon",
-	// 	BaseDamage: 180,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Cannon{Style: behaviors.CannonStyleMCannon, Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      3,
-	// 	Name:       "AirShot",
-	// 	BaseDamage: 20,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.AirShot{Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      4,
-	// 	Name:       "Vulcan1",
-	// 	BaseDamage: 10,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Vulcan{Shots: 3, Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      5,
-	// 	Name:       "Vulcan2",
-	// 	BaseDamage: 15,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Vulcan{Shots: 4, Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      6,
-	// 	Name:       "Vulcan3",
-	// 	BaseDamage: 20,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Vulcan{Shots: 5, Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      7,
-	// 	Name:       "SuprVulc",
-	// 	BaseDamage: 20,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Vulcan{Shots: 10, Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      73,
-	// 	Name:       "WideBlde",
-	// 	BaseDamage: 150,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Sword{Damage: damage, Style: behaviors.SwordStyleBlade, Range: behaviors.SwordRangeWide}
-	// 	},
-	// },
-	// {
-	// 	Index:      79,
-	// 	Name:       "WindRack",
-	// 	BaseDamage: 140,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.WindRack{Damage: damage}
-	// 	},
-	// },
-	// {
-	// 	Index:      160,
-	// 	Name:       "Recov300",
-	// 	BaseDamage: 0,
-	// 	MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-	// 		return &behaviors.Recov{HP: 300}
-	// 	},
-	// },
 	{
-		Index: 162,
-		Name:  "AreaGrab",
+		Index:      0,
+		Name:       "Cannon",
+		BaseDamage: 40,
 		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-			return &behaviors.AreaGrab{}
+			return &behaviors.Cannon{Style: behaviors.CannonStyleCannon, Damage: damage}
 		},
 	},
 	{
-		Index: 162,
-		Name:  "AreaGrab",
+		Index:      1,
+		Name:       "HiCannon",
+		BaseDamage: 100,
 		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-			return &behaviors.AreaGrab{}
+			return &behaviors.Cannon{Style: behaviors.CannonStyleHiCannon, Damage: damage}
 		},
 	},
 	{
-		Index: 162,
-		Name:  "AreaGrab",
+		Index:      2,
+		Name:       "M-Cannon",
+		BaseDamage: 180,
 		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-			return &behaviors.AreaGrab{}
+			return &behaviors.Cannon{Style: behaviors.CannonStyleMCannon, Damage: damage}
 		},
 	},
 	{
-		Index: 162,
-		Name:  "AreaGrab",
+		Index:      3,
+		Name:       "AirShot",
+		BaseDamage: 20,
 		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-			return &behaviors.AreaGrab{}
+			return &behaviors.AirShot{Damage: damage}
 		},
 	},
 	{
-		Index: 162,
-		Name:  "AreaGrab",
+		Index:      4,
+		Name:       "Vulcan1",
+		BaseDamage: 10,
 		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
-			return &behaviors.AreaGrab{}
+			return &behaviors.Vulcan{Shots: 3, Damage: damage}
+		},
+	},
+	{
+		Index:      5,
+		Name:       "Vulcan2",
+		BaseDamage: 15,
+		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
+			return &behaviors.Vulcan{Shots: 4, Damage: damage}
+		},
+	},
+	{
+		Index:      6,
+		Name:       "Vulcan3",
+		BaseDamage: 20,
+		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
+			return &behaviors.Vulcan{Shots: 5, Damage: damage}
+		},
+	},
+	{
+		Index:      7,
+		Name:       "SuprVulc",
+		BaseDamage: 20,
+		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
+			return &behaviors.Vulcan{Shots: 10, Damage: damage}
+		},
+	},
+	{
+		Index:      73,
+		Name:       "WideBlde",
+		BaseDamage: 150,
+		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
+			return &behaviors.Sword{Damage: damage, Style: behaviors.SwordStyleBlade, Range: behaviors.SwordRangeWide}
+		},
+	},
+	{
+		Index:      79,
+		Name:       "WindRack",
+		BaseDamage: 140,
+		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
+			return &behaviors.WindRack{Damage: damage}
+		},
+	},
+	{
+		Index:      160,
+		Name:       "Recov300",
+		BaseDamage: 0,
+		MakeBehavior: func(damage state.Damage) state.EntityBehavior {
+			return &behaviors.Recov{HP: 300}
 		},
 	},
 	{
