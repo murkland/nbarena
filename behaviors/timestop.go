c2575ff60f6aa44dc6e538c340d9fe61601ff0dc
bigfarts <bigfarts@punymail.com>
2022-03-16 09:32:57 -0700

    add cryptic note

--- behaviors/timestop.go
+++ behaviors/timestop.go
@@ -18,3 +18,5 @@ func (eb *Timestop) Cleanup(e *state.Entity, s *state.State) {
 	s.IsInTimeStop = false
 	e.RunsInTimestop = false
 }
+
+// 12725 -> 12743 (text appears) -> 12750 (text completes) -> 12793 (text starts disappearing) -> 12801 (text disappears) -> 12803 (action start) -> 12844 (Action end) -> 12882 (tf end)
